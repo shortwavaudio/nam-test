adf6ed8797aaf9a322171adc76f877058b1b2e3b
Stephane Pericat <shortwavlabs@StephanesLaptop.attlocal.net>
2024-09-01 16:12:15 -0500

    WDL

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -66,4 +66,7 @@ public:
 private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NamtestAudioProcessor)
+    
+//    WDL_String mNAMPath;
+    std::unique_ptr<ResamplingNAM> mModel;
 };
